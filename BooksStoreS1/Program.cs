decb1cb828d17f3ee32f709ea6e4c7be48b153f8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-20 12:45:01 +0530

    Changes

--- BooksStoreS1/Program.cs
+++ BooksStoreS1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HeaderFooter.Interfaces;
+using HeaderFooter;
+using Microsoft.Extensions.Configuration;
+
+var _configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddUserSecrets<Program>()
+    .Build();
+
+
+new Header().DisplayHeader('=', "Books Store");
+
+
+new Footer().DisplayFooter('-');
