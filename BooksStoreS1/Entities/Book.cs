48cd00084dab3aefbe8e2856af2d317729cc8517
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-20 12:51:04 +0530

    Create Book.cs

---
+++ BooksStoreS1/Entities/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace BooksStoreS1.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Author { get; set; } = string.Empty;
+
+    public DateTime PublishedOn { get; set; }
+
+    public string ISBN { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Pages { get; set; }
+
+    public string PictureUrl { get; set; } = string.Empty;
+}
