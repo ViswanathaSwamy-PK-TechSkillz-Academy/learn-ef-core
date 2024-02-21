1c75ce93d70602e2eab3e982eaab93c9db5c73d7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-21 08:38:00 +0530

    Swamy/20feb work (#2)
    * Initial Version
    
    * Changes
    
    * Create Book.cs
    
    * Changes
    
    * Update Program.cs
    
    * Update Program.cs
    
    * Update Program.cs
    
    * Update Program.cs

---
+++ BooksStoreS1/Persistence/BooksStoreDbContext.cs
@@ -0,0 +1,9 @@
+﻿using BooksStoreS1.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksStoreS1.Persistence;
+
+public class BooksStoreDbContext(DbContextOptions<BooksStoreDbContext> options) : DbContext(options)
+{
+    public DbSet<Book> Books => Set<Book>();
+}
