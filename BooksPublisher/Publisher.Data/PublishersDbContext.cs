c28ea9bb5ea955c775220ba9990c972a0a14bdf9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-21 23:20:06 +0530

    Changes

---
+++ BooksPublisher/Publisher.Data/PublishersDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Publisher.Domain;
+
+namespace Publisher.Data;
+
+public class PublishersDbContext(DbContextOptions<PublishersDbContext> options) : DbContext(options)
+{
+    public DbSet<Author> Authors => Set<Author>();
+
+    public DbSet<Book> Books => Set<Book>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // It is a BIG NO NO to hard code the connection string in the code
+        // optionsBuilder.UseSqlServer("Your Local Db Connection String");
+    }
+}
--- BooksPublisher/Publisher.Domain/Author.cs
+++ BooksPublisher/Publisher.Domain/Author.cs
@@ -5,7 +5,7 @@ public class Author
     public int Id { get; set; }
 
     public string FirstName { get; set; } = string.Empty;
-    
+
     public string LastName { get; set; } = string.Empty;
 
     public List<Book> Books { get; set; } = [];
