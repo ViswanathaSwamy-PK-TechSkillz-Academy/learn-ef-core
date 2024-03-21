529fb093a049e68937718a2f73d3d3ad101afe9c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-21 21:08:50 +0530

    Initial Version

---
+++ BooksPublisher/PublisherApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using HeaderFooter;
+using Microsoft.Extensions.Configuration;
+
+new Header().DisplayHeader('=', "Books Publisher");
+
+new Footer().DisplayFooter('-');
+
+static IConfigurationRoot GetConfigurationRoot()
+{
+    return new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddUserSecrets<Program>()
+        .Build();
+}
