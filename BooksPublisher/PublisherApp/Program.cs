e7c4847435e6dc5e714d6e51ff3a7c914f50958c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-21 23:29:59 +0530

    Changes

--- BooksPublisher/PublisherApp/Program.cs
+++ BooksPublisher/PublisherApp/Program.cs
@@ -1,9 +1,32 @@
 ﻿using HeaderFooter;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Publisher.Data;
+
 
 new Header().DisplayHeader('=', "Books Publisher");
 
+using PublishersDbContext _context = GetPublishersDbContext();
+
+ForegroundColor = ConsoleColor.DarkMagenta;
+WriteLine("Database is being created...");
+_context.Database.EnsureCreated();
+WriteLine("Database is created.");
+
+ForegroundColor = ConsoleColor.DarkCyan;
+
 new Footer().DisplayFooter('-');
+
+static PublishersDbContext GetPublishersDbContext()
+{
+    IConfigurationRoot _configuration = GetConfigurationRoot();
+
+    DbContextOptions<PublishersDbContext> dbContextOptions = new DbContextOptionsBuilder<PublishersDbContext>()
+    .UseSqlServer(_configuration.GetConnectionString("PublishersDbContext"))
+    .Options;
+
+    return new(dbContextOptions);
+}
 
 static IConfigurationRoot GetConfigurationRoot()
 {
