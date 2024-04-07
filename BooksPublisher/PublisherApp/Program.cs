c05cf8ae6231c1c923436b4946781acbc4a594e8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-07 16:10:03 +0530

    Update Program.cs

--- BooksPublisher/PublisherApp/Program.cs
+++ BooksPublisher/PublisherApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using HeaderFooter;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Publisher.Data;
+using Publisher.Domain;
 using PublisherApp.Configuration;
 using PublisherApp.Data;
 
@@ -12,8 +14,31 @@ WriteLine("Database is being created...");
 _context.Database.EnsureCreated();
 WriteLine("Database is created.\n");
 
+ForegroundColor = ConsoleColor.DarkGreen;
+WriteLine("Adding author to the database...");
+AddAuthor(new Author { FirstName = "John", LastName = "Doe" });
+WriteLine("Successfully added author to the database...");
+
 new Header().DisplayHeader('=', "Books Publisher");
 
 ForegroundColor = ConsoleColor.DarkCyan;
 
+GetAuthors();
+
 new Footer().DisplayFooter('-');
+
+void AddAuthor(Author author)
+{
+    _context.Authors.Add(author);
+
+    _ = _context.SaveChanges();
+}
+
+void GetAuthors()
+{
+    var authors = _context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
